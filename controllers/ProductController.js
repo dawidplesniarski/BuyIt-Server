ab0f1803cfe8f1f036de8beeb124083a92b52326
Rafał <rgebica@gmail.com>
2020-04-20 16:56:17 +0200

    Added order model

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -35,13 +35,11 @@ const product = {
     });
 
     try {
-      const savedProduct = await newProduct.save();
-      const updatedAllProducts = await Product.find();
-      console.log(updatedAllProducts);
-      socket.getIO().emit('productAdded', {savedProduct: savedProduct});
+      const addedProduct = await newProduct.save();
 
+      socket.getIO().emit('productAdded', {addedProduct: addedProduct});
 
-      res.status(200).send(savedProduct);
+      res.status(200).send(addedProduct);
     } catch (error) {
       res.status(500).send(error);
     }
